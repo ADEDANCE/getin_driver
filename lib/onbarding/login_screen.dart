500e995e81cb5567abaf2eacd1bf8171e0d67ddd
Adedance <uthmanadedance@gmail.com>
2026-02-28 16:55:08 +0100

    Add sign up route

--- lib/onbarding/login_screen.dart
+++ lib/onbarding/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:getin_driver/onbarding/signup_screen.dart';
+import 'package:getin_driver/reusable_sidget/button_widget.dart';
+import 'package:getin_driver/reusable_sidget/textfied_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,6 +12,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,26 +21,173 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Padding(
           padding: EdgeInsets.all(16),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset('assets/images/getin_logo.png', scale: 5),
-              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        'assets/images/getin_logo.png',
+                        scale: 3,
+                      ),
+                    ),
 
-              Text(
-                "GETIN",
-                style: TextStyle(
-                  fontSize: 52,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF3D4CF5),
+                    Text(
+                      "GETIN",
+                      style: TextStyle(
+                        fontSize: 52,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF3D4CF5),
+                      ),
+                    ),
+                    SizedBox(height: 15.h),
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                        fontSize: 40.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF000000),
+                      ),
+                    ),
+                    Text(
+                      "Sign in to start accepting rides",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF000000),
+                      ),
+                    ),
+                    SizedBox(height: 20.h),
+                    TextfiedWidget(
+                      controller: _emailcontroller,
+                      hintText: 'Email address',
+                    ),
+                    SizedBox(height: 20.h),
+                    TextfiedWidget(
+                      controller: _passwordcontroller,
+                      hintText: 'Password',
+                    ),
+                    SizedBox(height: 25.h),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Text(
+                        "Forgot password?",
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF3D4CF5),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 15.h),
+              ButtonWidget(
+                onPressed: () {},
+                backgroundColor: Color(0xFF3D4CF5),
+                height: 65.h,
+                width: double.infinity,
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(color: Color(0xFFa6a3a2), thickness: 1),
+                  ),
+                  SizedBox(width: 3),
+                  Text(
+                    "or continue with",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  Expanded(
+                    child: Divider(color: Color(0xFFa6a3a2), thickness: 1),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              ButtonWidget(
+                onPressed: () {},
+                backgroundColor: Color(0xFFFFFFFF),
+                bordercolor: Color(0xFFa6a3a2),
+                height: 40,
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/google-logo.png",
+                      width: 20,
+                      height: 20,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "GOOGLE",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(
+                      fontSize: 20.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF000000),
+                    ),
+                  ),
+                  SizedBox(width: 3),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => SignupScreen()),
+                      );
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF3D4CF5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25.h),
               Text(
-                "Welcome Back",
+                "By continuing, you agree to our Terms of Service and Privacy Policy",
+                textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
                   color: Color(0xFF000000),
                 ),
               ),
