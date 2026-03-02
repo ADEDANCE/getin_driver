53a06cf6ae0edfcd2790dc36741641be571d8b89
Adedance <uthmanadedance@gmail.com>
2026-03-02 20:23:24 +0100

    Feat(Create earnings_screen)

---
+++ lib/home/earnings_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EarningsScreengit extends StatefulWidget {
+  const EarningsScreengit({super.key});
+
+  @override
+  State<EarningsScreengit> createState() => _EarningsScreengitState();
+}
+
+class _EarningsScreengitState extends State<EarningsScreengit> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
