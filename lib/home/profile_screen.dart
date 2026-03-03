ca5a59fe6a97edbbd23429e9d232d303b8de8f2b
Adedance <uthmanadedance@gmail.com>
2026-03-03 18:35:05 +0100

    Refractor(Add Personal and car infomation section)

--- lib/home/profile_screen.dart
+++ lib/home/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:getin_driver/reusable_widget/details_container.dart';
+import 'package:getin_driver/reusable_widget/info_container.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -35,181 +37,181 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.r),
-                  color: Color(0xFFFFFFFF),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(
-                        alpha: 0.2,
-                      ), // shadow color
-                      blurRadius: 10, // how soft the shadow is
-                      spreadRadius: 2, // how wide it spreads
-                      offset: Offset(0, 4), // position (x, y)
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundColor: Color(0xFF9db0f5),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xFF3D4CF5),
-                        child: Text(
-                          "DR",
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16.r),
+                    color: Color(0xFFFFFFFF),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(
+                          alpha: 0.2,
+                        ), // shadow color
+                        blurRadius: 10, // how soft the shadow is
+                        spreadRadius: 1, // how wide it spreads
+                        offset: Offset(0, 2), // position (x, y)
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Color(0xFF9db0f5),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Color(0xFF3D4CF5),
+                          child: Text(
+                            "DR",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      "Alex Johnson",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                      Text(
+                        "Alex Johnson",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 3,
-                          ),
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 252, 241, 206),
-                            // border: Border.all(color: Color, width: 1.5),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Row(
-                            children: [
-                              Icon(Icons.star, color: Colors.amber, size: 18),
-                              SizedBox(width: 3.w),
-                              Text(
-                                "4.9",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 3,
+                            ),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 252, 241, 206),
+                              // border: Border.all(color: Color, width: 1.5),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(Icons.star, color: Colors.amber, size: 18),
+                                SizedBox(width: 3.w),
+                                Text(
+                                  "4.9",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 3,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.green, width: 1.5),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: const [
-                              Icon(
-                                Icons.workspace_premium,
+                          SizedBox(width: 10),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 3,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
                                 color: Colors.green,
-                                size: 18,
+                                width: 1.5,
                               ),
-                              SizedBox(width: 6),
-                              Text(
-                                "Top Driver",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16.r),
-                              color: Color.fromARGB(255, 220, 226, 245),
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            child: Column(
-                              children: [
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
                                 Icon(
-                                  Icons.trending_up,
-                                  color: Color(0xFF3D4CF5),
+                                  Icons.workspace_premium,
+                                  color: Colors.green,
+                                  size: 18,
                                 ),
+                                SizedBox(width: 6),
                                 Text(
-                                  "247",
+                                  "Top Driver",
                                   style: TextStyle(
                                     fontSize: 14,
-                                    color: Colors.black,
+                                    color: Colors.green,
                                     fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  "Total Rides",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.black,
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: Container(
-                            width: double.infinity,
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16.r),
-                              color: const Color.fromARGB(255, 241, 248, 241),
-                            ),
-                            child: Column(
-                              children: [
-                                Icon(Icons.calendar_month, color: Colors.green),
-                                Text(
-                                  "January 2025",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  "Member Since",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ],
+                        ],
+                      ),
+                      SizedBox(height: 10.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: DetailsContainer(
+                              iconmainAxisAlignment: MainAxisAlignment.center,
+                              backgroundColor: Color.fromARGB(
+                                255,
+                                220,
+                                226,
+                                245,
+                              ),
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              detailicon: Icon(
+                                Icons.trending_up,
+                                color: Color(0xFF3D4CF5),
+                              ),
+                              title: "247",
+                              subtitle: "Total Rides",
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: DetailsContainer(
+                              iconmainAxisAlignment: MainAxisAlignment.center,
+                              backgroundColor: const Color.fromARGB(
+                                255,
+                                241,
+                                248,
+                                241,
+                              ),
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              detailicon: Icon(
+                                Icons.calendar_month,
+                                color: Colors.green,
+                              ),
+                              title: "January 2025",
+                              subtitle: "Member Since",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 35.h),
+                InfoContainer(
+                  icon: Icon(Icons.person_2_outlined),
+                  title: "Personal Information",
+                  hitname: "Full Name",
+                  name: "Alex johnson",
+                  email: "alex.johnson@email.com",
+                  phone: "+234 904-123-4567",
+                  address: "123 Main Street, Downtown, CA 90210",
+                ),
+                SizedBox(height: 35.h),
+                //Vehicle Information
+                InfoContainer(
+                  icon: Icon(Icons.directions_car),
+                  title: "Vehicle Information",
+                  hitname: "Vehicle Model",
+                  name: "Toyota Camry 2020",
+                  type: "Sedan",
+                  license: "ABC 1234",
+                  color: "Black",
+                ),
+              ],
+            ),
           ),
         ),
       ),
